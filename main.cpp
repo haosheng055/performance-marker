ddd13523d1697cd4d388c46c64c44c2e78c5d047
haosheng <sheng.hao@duobei.com>
2021-06-16 18:02:19 +0800

    chore:init project

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "src/Bucket.h"
+#include "src/BucketedTimeSeries.h"
+#include "src/MultiLevelTimeSeries.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    Bucket<int> bucket;
+    return 0;
+}
---
+++ src/MultiLevelTimeSeries.h
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2018 Duobei Brothers Information Technology Co.,Ltd. All rights reserved.
+ *
+ * Author: haosheng (sheng.hao@duobei.com)
+ *
+ * Date: 2021/6/16
+ *
+ */
+
+#ifndef PERFORMANCE_MULTILEVELTIMESERIES_H
+#define PERFORMANCE_MULTILEVELTIMESERIES_H
+
+#endif//PERFORMANCE_MULTILEVELTIMESERIES_H
