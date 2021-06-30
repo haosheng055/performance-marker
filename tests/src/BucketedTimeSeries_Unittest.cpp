d8cccee10b90fb73e553f965b5ac17a05cd910ed
haosheng <sheng.hao@duobei.com>
2021-06-30 18:23:01 +0800

    test: 增加对3种容器类的单元测试

---
+++ tests/src/BucketedTimeSeries_Unittest.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Hao sheng on 2021/6/30.
+//
+#include "../../include/BucketedTimeSeries.h"
+#include "gtest/gtest.h"
+
+#include <thread>
+
+class BucketedTimeSeriesTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        auto beginTime = std::chrono::steady_clock::now();
+        bucketedTimeSeries1.addValue(beginTime, 10000);
+        bucketedTimeSeries1.addValue(beginTime + std::chrono::seconds(10), 1);
+        bucketedTimeSeries1.addValue(beginTime + std::chrono::seconds(11), 2);
+        bucketedTimeSeries1.addValue(beginTime + std::chrono::seconds(12), 3);
+    }
+    BucketedTimeSeries<double> bucketedTimeSeries1{10,std::chrono::seconds(10)};
+};
+
+TEST_F(BucketedTimeSeriesTest, addValue)
+{
+    EXPECT_EQ(bucketedTimeSeries1.count(), 3);
+    EXPECT_EQ(bucketedTimeSeries1.sum(), 6);
+    EXPECT_EQ(bucketedTimeSeries1.avg(), 2);
+    EXPECT_EQ(bucketedTimeSeries1.rate(),0.6);
+    EXPECT_EQ(bucketedTimeSeries1.countRate(), 0.3);
+}
+
+
---
+++ tests/src/MultiLevelTimeSeries_Unittest.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Hao sheng on 2021/6/30.
+//
+#include "../../include/MultiLevelTimeSeries.h"
+#include "gtest/gtest.h"
+
+#include <thread>
+
+class MultiLevelTimeSeriesTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        auto beginTime = std::chrono::steady_clock::now();
+        auto nextTime = beginTime + std::chrono::seconds(10);
+        multiLevelTimeSeries1.addValue(beginTime, 1000);
+        multiLevelTimeSeries1.addValue(nextTime, 1);
+        multiLevelTimeSeries1.addValue(nextTime, 2);
+        multiLevelTimeSeries1.addValue(nextTime, 3);
+        multiLevelTimeSeries1.update(nextTime);
+    }
+    MultiLevelTimeSeries<double> multiLevelTimeSeries1 { 10, { std::chrono::seconds(10), std::chrono::minutes(1) } };
+};
+
+TEST_F(MultiLevelTimeSeriesTest, addValueIn10Sec)
+{
+    EXPECT_EQ(multiLevelTimeSeries1.count(0), 3);
+    EXPECT_EQ(multiLevelTimeSeries1.sum(0), 6);
+    EXPECT_EQ(multiLevelTimeSeries1.avg(0), 2);
+    EXPECT_EQ(multiLevelTimeSeries1.rate(0), 0.6);
+    EXPECT_EQ(multiLevelTimeSeries1.countRate(0), 0.3);
+}
+
+TEST_F(MultiLevelTimeSeriesTest, addValueIn1Min)
+{
+    EXPECT_EQ(multiLevelTimeSeries1.count(1), 4);
+    EXPECT_EQ(multiLevelTimeSeries1.sum(1), 1006);
+    EXPECT_EQ(multiLevelTimeSeries1.avg(1), 251.5);
+}
+
