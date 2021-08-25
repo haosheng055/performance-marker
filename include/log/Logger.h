eea3e2fc102633cdebeef857c9af4e0f10d905ae
haosheng <sheng.hao@duobei.com>
2021-08-25 11:04:41 +0800

    feat: logger front end

--- include/log/Logger.h
+++ include/log/Logger.h
@@ -5,4 +5,84 @@
 #ifndef PERFORMANCEMARKER_LOGGER_H
 #define PERFORMANCEMARKER_LOGGER_H
 
-#endif //PERFORMANCEMARKER_LOGGER_H
+#include <chrono>
+#include <sstream>
+#include <string>
+
+#include "log/TimeStamp.h"
+
+class Logger {
+public:
+    enum LogLevel {
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR,
+        FALTAL
+    };
+
+    class BaseFileName {
+    public:
+        template <int N>
+        BaseFileName(const char (&fileName)[N])
+            : mFileName(fileName)
+            , mSize(N - 1)
+        {
+            auto result = strrchr(mFileName, '\\');
+            if (result) {
+                mFileName = result + 1;
+                mSize -= (mFileName - fileName);
+            }
+        }
+        explicit BaseFileName(const char* fileName)
+            : mFileName(fileName)
+        {
+            auto result = strrchr(fileName, '\\');
+            if (result) {
+                mFileName = result + 1;
+            }
+            mSize = static_cast<uint32_t>(strlen(mFileName));
+        }
+        const char* getFileName() const
+        {
+            return mFileName;
+        }
+
+    private:
+        const char* mFileName; // non-path
+        uint32_t mSize;
+    };
+
+    Logger(BaseFileName file, int line);
+    Logger(BaseFileName file, int line, LogLevel level);
+    ~Logger();
+
+    static LogLevel getLogLevel();
+    static void setLogLevel(LogLevel level);
+
+    typedef void (*OutputFunc)(const char* msg, int len);
+    static void setOutput(OutputFunc outputFunc);
+
+    std::stringstream& getStream() { return mImpl.mStream; }
+
+private:
+    class Impl {
+    public:
+        Impl(LogLevel level, const BaseFileName& file, int line);
+        void addSourceFile();
+        Timestamp mTimestamp;
+        std::stringstream mStream;
+        LogLevel mLevel;
+        int mLine;
+        BaseFileName mBaseFileName;
+    };
+    Impl mImpl;
+};
+
+#define LOG_DEBUG Logger(__FILE__, __LINE__, Logger::DEBUG).getStream()
+#define LOG_INFO Logger(__FILE__, __LINE__).getStream()
+#define LOG_WARN Logger(__FILE__, __LINE__, Logger::WARN).getStream()
+#define LOG_ERROR Logger(__FILE__, __LINE__, Logger::ERROR).getStream()
+#define LOG_FATAL Logger(__FILE__, __LINE__, Logger::FATAL).getStream()
+
+#endif // PERFORMANCEMARKER_LOGGER_H
--- src/log/Logger.cpp
+++ src/log/Logger.cpp
@@ -2,4 +2,71 @@
 // Created by DELL on 2021/8/24.
 //
 
-#include "Logger.h"
+#include <thread>
+
+#include "log/Logger.h"
+
+const char* LogLevelName[] = {
+    "DEBUG ",
+    "INFO  ",
+    "WARN  ",
+    "ERROR ",
+    "FATAL "
+};
+
+void defaultOutput(const char* msg, int len)
+{
+    size_t n = fwrite(msg, 1, len, stdout);
+}
+
+Logger::OutputFunc g_output = defaultOutput;
+Logger::LogLevel g_logLevel = Logger::INFO;
+
+Logger::Impl::Impl(Logger::LogLevel level, const Logger::BaseFileName& file, int line)
+: mLevel(level)
+, mTimestamp(Timestamp::now())
+, mStream()
+, mBaseFileName(file)
+, mLine(line)
+{
+    mStream << mTimestamp.toFixedSizeString() << ' ';
+    mStream << LogLevelName[mLevel];
+    mStream << std::this_thread::get_id() << ' ';
+}
+
+void Logger::Impl::addSourceFile()
+{
+    mStream << " - " << mBaseFileName.getFileName() << ':' << mLine << '\n';
+}
+
+Logger::Logger(Logger::BaseFileName file, int line)
+    : mImpl(Logger::INFO, file, line)
+{
+}
+
+Logger::Logger(Logger::BaseFileName file, int line, Logger::LogLevel level)
+    : mImpl(level, file, line)
+{
+}
+
+Logger::~Logger()
+{
+    mImpl.addSourceFile();
+    auto str = mImpl.mStream.str();
+    g_output(str.c_str(), str.size());
+}
+
+Logger::LogLevel Logger::getLogLevel()
+{
+    return g_logLevel;
+}
+
+void Logger::setLogLevel(Logger::LogLevel level)
+{
+    g_logLevel = level;
+}
+
+void Logger::setOutput(Logger::OutputFunc outputFunc)
+{
+    g_output = outputFunc;
+}
