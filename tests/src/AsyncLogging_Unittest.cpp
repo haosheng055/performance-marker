6d12b57641d4ac370e70260faf43264a7ad24d33
haosheng <sheng.hao@duobei.com>
2021-08-25 11:05:55 +0800

    test: AsyncLoggingTest

--- tests/src/AsyncLogging_Unittest.cpp
+++ tests/src/AsyncLogging_Unittest.cpp
@@ -1,4 +1,43 @@
 //
 // Created by DELL on 2021/8/25.
 //
+#include <chrono>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <thread>
 
+#include <log/AsyncLogging.h>
+#include <log/Logger.h>
+
+using namespace std;
+
+AsyncLogging* g_asyncLog = nullptr;
+
+void asyncOutput(const char* msg, int len)
+{
+    g_asyncLog->append(msg, len);
+}
+
+void bench()
+{
+    Logger::setOutput(asyncOutput);
+
+    string line = "1234567890 abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+    for (int i = 0; i < 10000; ++i)
+    {
+        LOG_INFO << line << i;
+        this_thread::sleep_for(100ms);
+    }
+}
+
+TEST(AsyncLoggingTest, bench)
+{
+    printf("pid = %d\n", getpid());
+
+    string fileName = "AsyncLoggingTest";
+    AsyncLogging log(fileName, 10);
+    g_asyncLog = &log;
+
+    bench();
+}
--- tests/src/LogFile_unittest.cpp
+++ tests/src/LogFile_unittest.cpp
@@ -1,4 +1,35 @@
 //
 // Created by DELL on 2021/8/25.
 //
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <thread>
+#include <chrono>
+
+#include "log/LogFile.h"
+#include "log/Logger.h"
+
+using namespace std;
+
+std::unique_ptr<LogFile> g_logFile;
+
+void outputFunc(const char* msg, int len)
+{
+    g_logFile->append(msg, len);
+}
+
+TEST(LogFileTest, bench)
+{
+    string fileName = "LogFileTest";
+    g_logFile.reset(new LogFile(fileName, 200*1000, 10));
+    Logger::setOutput(outputFunc);
+
+    string line = "1234567890 abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+    for (int i = 0; i < 10000; ++i)
+    {
+        LOG_INFO << line << i;
+        this_thread::sleep_for(100ms);
+    }
+}
 
