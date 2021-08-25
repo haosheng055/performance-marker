08e2001bf1da456d769131be4d134eab52daf00a
haosheng <sheng.hao@duobei.com>
2021-08-25 11:05:29 +0800

    feat: AsyncLog

--- include/log/AsyncLogging.h
+++ include/log/AsyncLogging.h
@@ -5,12 +5,50 @@
 #ifndef PERFORMANCEMARKER_ASYNCLOGGING_H
 #define PERFORMANCEMARKER_ASYNCLOGGING_H
 
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <sstream>
+#include <thread>
+#include <vector>
 
+#include "LogFile.h"
 
+// two buffers; func: append(), threadFuc()
+// append(): when buffer A is full(?), notify the looping thread to log
+// all the full buffers and the buffer being written.
+// threadFunc(): log buffers, swap buffer B buffer A
 class AsyncLogging {
+public:
+    AsyncLogging(std::string& fileName, int flushInterval);
 
+    void append(const char* logLine, int len);
+
+    void threadFunc();
+
+    void stop()
+    {
+        mIsRunning = false;
+        mCondition.notify_one();
+        mThread.join();
+    }
+
+private:
+    const int kFixedSize = 1000;
+    const int kFlushInterval;
+    std::string mFileName;
+    std::atomic<bool> mIsRunning;
+    std::thread mThread;
+
+    using Buffer = std::stringstream;
+    using BufferPtr = std::unique_ptr<Buffer>;
+    using BufferVector = std::vector<BufferPtr>;
+    std::mutex mMutex;
+    std::condition_variable mCondition;
+    BufferPtr mCurrentBuffer;
+    BufferVector mBuffers;
 };
 
-
-
-#endif //PERFORMANCEMARKER_ASYNCLOGGING_H
+#endif // PERFORMANCEMARKER_ASYNCLOGGING_H
--- src/log/AsyncLogging.cpp
+++ src/log/AsyncLogging.cpp
@@ -1,5 +1,67 @@
 //
 // Created by DELL on 2021/8/25.
 //
+#include "log/AsyncLogging.h"
+#include <cassert>
 
-#include "AsyncLogging.h"
+using namespace std;
+
+AsyncLogging::AsyncLogging(string& fileName, int flushInterval)
+    : kFlushInterval(flushInterval)
+    , mFileName(fileName)
+    , mIsRunning(true)
+    , mThread([this]() { threadFunc(); })
+    , mMutex()
+    , mCondition()
+    , mCurrentBuffer(new Buffer)
+    , mBuffers()
+{
+    mBuffers.reserve(16);
+}
+
+void AsyncLogging::append(const char* logLine, int len)
+{
+    unique_lock<mutex> uniqueLock(mMutex);
+    if (mCurrentBuffer->str().size() + len < kFixedSize) {
+        (*mCurrentBuffer) << logLine;
+    } else {
+        mBuffers.push_back(move(mCurrentBuffer));
+        mCurrentBuffer.reset(new Buffer);
+        (*mCurrentBuffer) << logLine;
+        mCondition.notify_one();
+    }
+}
+
+void AsyncLogging::threadFunc()
+{
+    auto newBuffer = make_unique<Buffer>();
+    BufferVector bufferVector;
+    bufferVector.reserve(16);
+    LogFile logFile(mFileName, kFixedSize * 1000, kFlushInterval);
+    while (mIsRunning) {
+        assert(bufferVector.empty());
+        {
+            unique_lock<mutex> uniqueLock(mMutex);
+            if (mBuffers.empty()) {
+                mCondition.wait_for(uniqueLock, chrono::seconds(kFlushInterval));
+            }
+            mBuffers.push_back(move(mCurrentBuffer));
+            mCurrentBuffer = move(newBuffer);
+            bufferVector.swap(mBuffers);
+        }
+        assert(!bufferVector.empty());
+
+        for (auto& buffer : bufferVector) {
+            logFile.append(buffer->str().c_str(), buffer->str().size());
+        }
+        logFile.flush();
+
+        if (!newBuffer) {
+            newBuffer = move(bufferVector.back());
+            bufferVector.pop_back();
+            newBuffer->clear();
+        }
+        bufferVector.clear();
+    }
+    logFile.flush();
+}
